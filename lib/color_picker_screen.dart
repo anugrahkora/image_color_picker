d4d03efd47b81a9b523d91853b0d72fde0bf1774
Anugrah K <anugrahkorembeth.ak@gmail.com>
2022-08-17 00:46:50 +0530

    Added stream for color container's position

--- lib/color_picker_screen.dart
+++ lib/color_picker_screen.dart
@@ -7,6 +7,13 @@ import 'package:flutter/rendering.dart';
 import 'package:image/image.dart' as img;
 import 'package:flutter/services.dart' show rootBundle;
 
+class Marker {
+  final double x;
+  final double y;
+
+  Marker({this.x = 0.0, this.y = 0.0});
+}
+
 class ColorPickerScreen extends StatefulWidget {
   const ColorPickerScreen({Key? key}) : super(key: key);
 
@@ -19,20 +26,23 @@ class _ColorPickerScreenState extends State<ColorPickerScreen> {
   GlobalKey imageKey = GlobalKey();
   GlobalKey paintKey = GlobalKey();
   bool useSnapshot = false;
-GlobalKey currentKey = GlobalKey();
+  GlobalKey currentKey = GlobalKey();
 
   final StreamController<Color> _stateController = StreamController<Color>();
+  final StreamController<Marker> _gestureController =
+      StreamController<Marker>();
   img.Image? photo;
 
   @override
   void initState() {
     currentKey = useSnapshot ? paintKey : imageKey;
+    _gestureController.add(Marker(x: 0.0, y: 0.0));
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
+    // final size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(title: const Text("Color picker ")),
@@ -41,77 +51,92 @@ GlobalKey currentKey = GlobalKey();
             stream: _stateController.stream,
             builder: (buildContext, AsyncSnapshot<Color> snapshot) {
               Color selectedColor = snapshot.data ?? Colors.green;
-              return Stack(
-                children: <Widget>[
-                  RepaintBoundary(
-                    key: paintKey,
-                    child: GestureDetector(
-                      onPanDown: (details) {
-                        searchPixel(details.globalPosition);
-                      },
-                      onPanUpdate: (details) {
-                        searchPixel(details.globalPosition);
-                      },
-                      child: Center(
-                        child: Image.asset(
-                          imagePath,
-                          key: imageKey,
-                          //color: Colors.red,
-                          //colorBlendMode: BlendMode.hue,
-                          //alignment: Alignment.bottomRight,
-                          fit: BoxFit.contain,
-                          //scale: .8,
+              return StreamBuilder<Marker>(
+                  stream: _gestureController.stream,
+                  builder: (context, position) {
+                    return Stack(
+                      children: <Widget>[
+                        RepaintBoundary(
+                          key: paintKey,
+                          child: GestureDetector(
+                            onTapDown: (details) {
+                              searchPixel(details.globalPosition,
+                                  details.localPosition);
+                            },
+                            onPanDown: (details) {
+                              searchPixel(details.globalPosition,
+                                  details.localPosition);
+                            },
+                            onPanUpdate: (details) {
+                              searchPixel(details.globalPosition,
+                                  details.localPosition);
+                            },
+                            child: Center(
+                              child: Image.asset(
+                                imagePath,
+                                key: imageKey,
+                                //color: Colors.red,
+                                //colorBlendMode: BlendMode.hue,
+                                //alignment: Alignment.bottomRight,
+                                fit: BoxFit.contain,
+                                //scale: .8,
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.all(70),
-                    width: 50,
-                    height: 50,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: selectedColor,
-                        border: Border.all(width: 2.0, color: Colors.white),
-                        boxShadow: const [
-                          BoxShadow(
-                              color: Colors.black12,
-                              blurRadius: 4,
-                              offset: Offset(0, 2))
-                        ]),
-                  ),
-                  Positioned(
-                    left: 114,
-                    top: 95,
-                    child: Text('$selectedColor',
-                        style: const TextStyle(
-                            color: Colors.white,
-                            backgroundColor: Colors.black54)),
-                  ),
-                ],
-              );
+                        Positioned(
+                          left: position.data?.x,
+                          top: position.data?.y,
+                          child: Container(
+                            // margin: const EdgeInsets.all(70),
+                            width: 50,
+                            height: 50,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: selectedColor,
+                                border:
+                                    Border.all(width: 2.0, color: Colors.white),
+                                boxShadow: const [
+                                  BoxShadow(
+                                      color: Colors.black12,
+                                      blurRadius: 4,
+                                      offset: Offset(0, 2))
+                                ]),
+                          ),
+                        ),
+                        Positioned(
+                          left: 100.0,
+                          top: 100.0,
+                          child: Text('$selectedColor',
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  backgroundColor: Colors.black54)),
+                        ),
+                      ],
+                    );
+                  });
             }),
       ),
     );
   }
 
-  void searchPixel(Offset globalPosition) async {
+  void searchPixel(Offset globalPosition, Offset localPosition) async {
     if (photo == null) {
       await (useSnapshot ? loadSnapshotBytes() : loadImageBundleBytes());
     }
-    _calculatePixel(globalPosition);
+    _calculatePixel(globalPosition, localPosition);
   }
 
-  void _calculatePixel(Offset globalPosition) {
+  void _calculatePixel(Offset globalPosition, Offset gesturePosition) {
     RenderBox box = currentKey.currentContext!.findRenderObject() as RenderBox;
     Offset localPosition = box.globalToLocal(globalPosition);
+    // print("${localPosition.dx.toInt()}  ${localPosition.dy.toInt()}");
 
     double px = localPosition.dx;
     double py = localPosition.dy;
 
     if (!useSnapshot) {
       double widgetScale = box.size.width / photo!.width;
-      print(py);
       px = (px / widgetScale);
       py = (py / widgetScale);
     }
@@ -120,6 +145,8 @@ GlobalKey currentKey = GlobalKey();
     int hex = abgrToArgb(pixel32);
 
     _stateController.add(Color(hex));
+    _gestureController
+        .add(Marker(x: gesturePosition.dx, y: gesturePosition.dy));
   }
 
   Future<void> loadImageBundleBytes() async {
